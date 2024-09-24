4cc9d54a589a8749d604d7de633fb1a50af3cf73
sajidulhaque007 <sajidulhaque007@gmail.com>
2024-09-24 19:02:54 +0600

    ...

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import ECommerce from "@/components/Dashboard/E-commerce";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import React from "react";
-
 const Dashboard = () => {
   return (
     <div>
