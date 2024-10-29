3c7ac449e54667b26dd2a0ad1ea7d21c50dba67a
Tasnimul <tasnimulsharan8962@gmail.com>
2024-10-29 13:35:13 +0600

    updated deleteImageApi

--- src/redux/api/deleteImageApi.ts
+++ src/redux/api/deleteImageApi.ts
@@ -4,7 +4,9 @@ const API_URL = 'http://localhost:4000/api/v1';
 interface deleteElements{
     countryName: string,
     elementType: string,
-    elementIndex: string
+    elementIndex: number
+    documentType:string
+    iconIndex:number
 }
 
 
@@ -15,7 +17,7 @@ export const deleteVisaElement = async ({countryName, elementType, elementIndex}
   return response.data;
 };
 
-export const deleteVisaRequirementIcon = async ({countryName, documentType, iconIndex} deleteElements) => {
+export const deleteVisaRequirementIcon = async ({countryName, documentType, iconIndex} : deleteElements) => {
   const response = await axios.delete(
     `${API_URL}/visa/${countryName}/requirement/${documentType}/icon/${iconIndex}`
   );
