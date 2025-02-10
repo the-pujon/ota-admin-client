f110ff06dbabfefbedfe91b88f262328248ed520
sajidulhaque007 <sajidulhaque007@gmail.com>
2025-02-10 17:19:08 +0600

    added package module/add

--- src/app/(packages)/updatePackage/page.tsx
+++ src/app/(packages)/updatePackage/page.tsx
@@ -1,11 +1,19 @@
-// import React from 'react'
+"use client";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import AddPackage from "@/components/Package/AddPackage";
+import useRedirectHelper from "@/utils/authRedirectHelper";
 
-// const page = () => {
-//   return (
-//     <div>
-//       Update Package
-//     </div>
-//   )
-// }
+const Page = () => {
+  useRedirectHelper("/updatePackage");
+  return (
+    <DefaultLayout>
+      <div className="flex flex-col gap-10">
+        <Breadcrumb pageName="Edit Package" />
+        {/* <AddPackage /> */}
+      </div>
+    </DefaultLayout>
+  );
+};
 
-// export default page
+export default Page;
