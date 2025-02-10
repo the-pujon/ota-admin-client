cd88d2e18904808d3d97c6cdd761b3363714c12f
tripnesttech <sajidulhaque007@gmail.com>
2025-02-10 17:05:45 +0600

    deployed the package add form

--- src/app/(packages)/allPackage/page.tsx
+++ src/app/(packages)/allPackage/page.tsx
@@ -1,47 +1,47 @@
-"use client"
-import React from 'react';
-import { useForm, useFieldArray, Controller } from 'react-hook-form';
+// "use client"
+// import React from 'react';
+// import { useForm, useFieldArray, Controller } from 'react-hook-form';
 
-function MyForm() {
-  const { control, handleSubmit } = useForm({
-    defaultValues: {
-      users: [{ name: '' }], // Ensure at least one field is initialized
-    },
-  });
+// function MyForm() {
+//   const { control, handleSubmit } = useForm({
+//     defaultValues: {
+//       users: [{ name: '' }], // Ensure at least one field is initialized
+//     },
+//   });
 
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: 'users', // Name of the field array
-  });
+//   const { fields, append, remove } = useFieldArray({
+//     control,
+//     name: 'users', // Name of the field array
+//   });
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+//   const onSubmit = (data:any) => {
+//     console.log(data);
+//   };
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {fields.map((field, index) => (
-        <div key={field.id}>
-          <Controller
-            name={`users[${index}].name`}
-            control={control}
-            render={({ field }) => <input {...field} />}
-          />
-          {fields.length > 1 && (
-            <button type="button" onClick={() => remove(index)}>
-              Remove
-            </button>
-          )}
-        </div>
-      ))}
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//       {fields.map((field, index) => (
+//         <div key={field.id}>
+//           <Controller
+//             name={`users[${index}].name`}
+//             control={control}
+//             render={({ field }) => <input {...field} />}
+//           />
+//           {fields.length > 1 && (
+//             <button type="button" onClick={() => remove(index)}>
+//               Remove
+//             </button>
+//           )}
+//         </div>
+//       ))}
       
-      <button type="button" onClick={() => append({ name: '' })}>
-        Add Field
-      </button>
+//       <button type="button" onClick={() => append({ name: '' })}>
+//         Add Field
+//       </button>
       
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
 
-export default MyForm;
+// export default MyForm;
